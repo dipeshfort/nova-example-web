0aefa323251a74f00968985d2917b47ac73ac3c5
dipesh <dipesh.fort@gmail.com>
2018-08-10 01:36:57 +0300

    Update name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Reminder App' });
+  res.render('index', { title: 'Reminder App 2018' });
 });
 
 module.exports = router;
